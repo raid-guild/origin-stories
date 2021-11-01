3de591bae0a2c35ba1a65b4f849cc3b8a15ed17a
beyondr <jeopatto@gmail.com>
2021-11-01 00:11:00 -0500

    Switch to local font loading

--- pages/index.js
+++ pages/index.js
@@ -54,16 +54,6 @@ const Home = ({ stories }) => {
                     property="twitter:image"
                     content="/images/social-banner.png"
                 />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin="true"
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&family=Uncial+Antiqua&display=swap"
-                    rel="stylesheet"
-                />
             </Head>
             <div className="bg-dark-darker">
                 <div className="h-[650px] relative">
