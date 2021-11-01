96ba0405a03671267504ced2d6a1ba87de4d6a04
beyondr <jeopatto@gmail.com>
2021-10-31 23:36:55 -0500

    Update share image

--- pages/index.js
+++ pages/index.js
@@ -34,10 +34,7 @@ const Home = ({ stories }) => {
                     property="og:description"
                     content="Learn the origin stories of members from Raid Guild! Hear their backgrounds: how they got into Web3, and Raid Guild; their thoughts on DAOs and more."
                 />
-                <meta
-                    property="og:image"
-                    content="%PUBLIC_URL%/images/social-banner.png"
-                />
+                <meta property="og:image" content="/images/social-banner.png" />
 
                 {/* Twitter */}
                 <meta property="twitter:card" content="summary_large_image" />
@@ -55,7 +52,7 @@ const Home = ({ stories }) => {
                 />
                 <meta
                     property="twitter:image"
-                    content="%PUBLIC_URL%/images/social-banner.png"
+                    content="/images/social-banner.png"
                 />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link
