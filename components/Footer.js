27d363463da4020fbb1c4b7e600b09e1e7ad7335
Kevin Choi <kevinyoungchoi@gmail.com>
2021-11-05 08:53:54 -0400

    add additional stylying on footer and reduce height

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="flex items-center justify-center h-24 px-8 bg-primary bg-opacity-50">
+        <footer className="flex items-center justify-center h-16 px-8 bg-gradient-to-b from-dark via-primary opacity-80">
             <p>Made with ⚔️ for our fellow raiders</p>
         </footer>
     );
