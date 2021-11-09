b24054d9bfc7ec056ac842b0df0690c9526b61fe
beyondr <jeopatto@gmail.com>
2021-11-09 15:23:03 -0500

    Update card props and add Sam Bau bio

--- pages/origins/[slug].js
+++ pages/origins/[slug].js
@@ -11,111 +11,120 @@ import mapImage from '../../public/static/images/map.png';
 import { getAllStories } from '../../utils/data';
 
 export default function Article({
-	name,
-	date,
-	header,
-	username,
-	featured,
-	picture,
-	excerpt,
-	interviewer,
-	content,
+    name,
+    date,
+    header,
+    username,
+    bio,
+    featured,
+    picture,
+    excerpt,
+    interviewer,
+    content,
 }) {
-	const { ref, inView } = useInView({ threshold: 0.05, triggerOnce: true });
-	const props = {
-		picture: picture,
-		name: name,
-		excerpt: excerpt,
-		featured: featured,
-		inView: inView,
-	};
-	return (
-		<>
-			<Head>
-				<title>{name} | Raid Guid Origins</title>
-				<meta name='description' content={excerpt} />
-
-				{/* Twitter */}
-				<meta
-					property='twitter:card'
-					content='summary_large_image'
-					key='twcard'
-				/>
-
-				{/* Facebook */}
-				<meta
-					property='og:site_name'
-					content='Raid Guild Origins'
-					key='ogsitename'
-				/>
-				<meta
-					property='og:url'
-					content='https://portfolio.raidguild.org/'
-					key='ogurl'
-				/>
-				<meta property='og:image' content={`/story/${header}`} key='ogimage' />
-                    content={`https://raidguild-origins.vercel.app/static/images/story/${header}`}
-				<meta
-					property='og:title'
-					content={`${name} | Raid Guild Origins`}
-					key='ogtitle'
-				/>
-				<meta property='og:description' content={excerpt} key='ogdesc' />
-			</Head>
-			<div className='bg-dark-darker'>
-				<div className='relative'>
-					<div className='relative z-10'>
-						<Header />
-						<div className='px-4 md:px-40 lg:px-56 pt-4'>
-							<div className='aspect-w-11 aspect-h-5 filter drop-shadow-lg max-h-[650px]'>
-								<motion.div
-									animate={{
-										y: 0,
-										rotate: 0,
-										opacity: 1,
-									}}
-									initial={{
-										y: -100,
-										rotate: -6,
-										opacity: 0,
-									}}
-									transition={{
-										duration: 1.2,
-										delay: 1,
-									}}>
-									<Image
-										className='object-cover object-center rounded-md'
+    const { ref, inView } = useInView({ threshold: 0.05, triggerOnce: true });
+    return (
+        <>
+            <Head>
+                <title>{name} | Raid Guid Origins</title>
+                <meta name="description" content={excerpt} />
+                {/* Twitter */}
+                <meta
+                    property="twitter:card"
+                    content="summary_large_image"
+                    key="twcard"
+                />
+                {/* Facebook */}
+                <meta
+                    property="og:site_name"
+                    content="Raid Guild Origins"
+                    key="ogsitename"
+                />
+                <meta
+                    property="og:url"
+                    content="https://portfolio.raidguild.org/"
+                    key="ogurl"
+                />
+                <meta
+                    property="og:image"
+                    content={`/story/${header}`}
+                    key="ogimage"
+                />
+                content=
+                {`https://raidguild-origins.vercel.app/static/images/story/${header}`}
+                <meta
+                    property="og:title"
+                    content={`${name} | Raid Guild Origins`}
+                    key="ogtitle"
+                />
+                <meta
+                    property="og:description"
+                    content={excerpt}
+                    key="ogdesc"
+                />
+            </Head>
+            <div className="bg-dark-darker">
+                <div className="relative">
+                    <div className="relative z-10">
+                        <Header />
+                        <div className="px-4 md:px-40 lg:px-56 pt-4">
+                            {/* <div className="px-4 md:px-40 lg:px-56 pt-4"> */}
+                            <div className="aspect-w-11 aspect-h-5 filter drop-shadow-lg max-h-[650px]">
+                                <motion.div
+                                    animate={{
+                                        y: 0,
+                                        rotate: 0,
+                                        opacity: 1,
+                                    }}
+                                    initial={{
+                                        y: -100,
+                                        rotate: -6,
+                                        opacity: 0,
+                                    }}
+                                    transition={{
+                                        duration: 1.2,
+                                        delay: 1,
+                                    }}
+                                >
+                                    <Image
+                                        className="object-cover object-center rounded-md"
                                         src={`/static/images/story/${header}`}
-										alt={`${name} header illustration`}
-										layout='fill'
-										objectFit='cover'
-										sizes='70vw'
-										priority
-									/>
-								</motion.div>
-							</div>
-						</div>
-					</div>
-					<Image
-						src={mapImage}
-						className='object-cover object-bottom absolute inset-0 z-0'
-						layout='fill'
-						sizes='70vw'
-						quality='60'
-						draggable='false'
-						alt='Raid map'
-					/>
-					<div className='absolute inset-0 bg-gradient-to-b from-dark via-primary opacity-80 z-0'></div>
-				</div>
-				<div className='flex flex-wrap justify-center md:px-16 lg:px-20 gap-16 pt-8 h-full'>
-					<main className='order-2 xl:order-1 self-center z-[999] px-6'>
-						<h1 className='font-sans'>The Origin Story of {name}</h1>
-						<p className='text-base'>
-							<span className='opacity-70'>— From a talk with </span>
-							<span className='text-primary opacity-100'>{interviewer}</span>
-						</p>
+                                        alt={`${name} header illustration`}
+                                        layout="fill"
+                                        objectFit="cover"
+                                        sizes="70vw"
+                                        priority
+                                    />
+                                </motion.div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <Image
+                        src={mapImage}
+                        className="object-cover object-bottom absolute inset-0 z-0"
+                        layout="fill"
+                        sizes="70vw"
+                        quality="60"
+                        draggable="false"
+                        alt="Raid map"
+                    />
+                    <div className="absolute inset-0 bg-gradient-to-b from-dark via-primary opacity-80 z-0"></div> */}
+                </div>
+                <div className="flex flex-wrap justify-center md:px-16 lg:px-20 gap-16 pt-8 h-full">
+                    <main className="order-2 xl:order-1 self-center z-[999] px-6">
+                        <h1 className="font-sans">
+                            The Origin Story of {name}
+                        </h1>
+                        <p className="text-base">
+                            <span className="opacity-70">
+                                — From a talk with{' '}
+                            </span>
+                            <span className="text-primary opacity-100">
+                                {interviewer}
+                            </span>
+                        </p>
 
-						{/* <div className="py-4 w-1/2">
+                        {/* <div className="py-4 w-1/2">
                             <p>Listen the episode 👂</p>
                             <div className="flex justify-between text-sm text-grey-darker">
                                 <p>0:42</p>
@@ -130,44 +139,54 @@ export default function Article({
                             </div>
                         </div> */}
 
-						<article className='prose prose-lg my-8 font-sans'>
-							<MDXRemote {...content} />
-						</article>
-					</main>
+                        <article className="prose prose-lg my-8 font-sans">
+                            <MDXRemote {...content} />
+                        </article>
+                    </main>
 
-					<aside
-						ref={ref}
-						className='order-1 xl:order-2 xl:h-screen xl:sticky top-4 px-6 lg:px-0 lg:pb-0'>
-						<Card props={props} />
-					</aside>
-				</div>
-				<Footer />
-			</div>
-		</>
-	);
+                    <aside
+                        ref={ref}
+                        className="order-1 xl:order-2 xl:h-screen xl:sticky top-4 px-6 lg:px-0 lg:pb-0"
+                    >
+                        <Card
+                            props={{
+                                picture,
+                                name,
+                                excerpt,
+                                bio,
+                                featured,
+                                inView,
+                            }}
+                        />
+                    </aside>
+                </div>
+                <Footer />
+            </div>
+        </>
+    );
 }
 
 export async function getStaticProps(context) {
-	const { params } = context;
-	const allStories = getAllStories();
+    const { params } = context;
+    const allStories = getAllStories();
 
-	const { data, content } = allStories.find(
-		(story) => story.slug === params.slug
-	);
-	const mdxSource = await serialize(content);
+    const { data, content } = allStories.find(
+        (story) => story.slug === params.slug
+    );
+    const mdxSource = await serialize(content);
 
-	return {
-		props: { ...data, content: mdxSource },
-	};
+    return {
+        props: { ...data, content: mdxSource },
+    };
 }
 
 export async function getStaticPaths() {
-	return {
-		paths: getAllStories().map((story) => ({
-			params: {
-				slug: story.slug,
-			},
-		})),
-		fallback: false,
-	};
+    return {
+        paths: getAllStories().map((story) => ({
+            params: {
+                slug: story.slug,
+            },
+        })),
+        fallback: false,
+    };
 }
