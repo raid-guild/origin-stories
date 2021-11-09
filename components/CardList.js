379bd98b498aba689a3927cd67de7779b023d8cd
Nitegeist <delroybicknell@gmail.com>
2021-11-09 00:52:13 -0500

    Update card component styling & add card to story page

--- components/CardList.js
+++ components/CardList.js
@@ -6,21 +6,28 @@ export default function CardList({ stories }) {
 	const featuredStories = stories.sort((a, b) => {
 		return a.featured === b.featured ? 0 : a.featured ? -1 : 1;
 	});
+	const featuredStoryCards = featuredStories.map((story, index) => {
+		let props = {
+			name: story.name,
+			slug: story.slug,
+			header: story.header,
+			username: story.username,
+			featured: story.featured,
+			picture: story.picture,
+			excerpt: story.excerpt,
+			index: index,
+			inView: inView,
+			maxLength: stories.length,
+		};
+		return <Card key={index} props={props} />;
+	});
 	return (
 		<>
 			<h2 className='my-10'>Origin Stories</h2>
 			<div
 				ref={ref}
 				className='grid lg:grid-cols-2 xl:grid-cols-3 justify-items-center'>
-				{featuredStories.map((story, index) => (
-					<Card
-						key={index}
-						story={story}
-						index={index}
-						inView={inView}
-						maxLength={stories.length}
-					/>
-				))}
+				{featuredStoryCards}
 			</div>
 		</>
 	);
--- pages/origins/[slug].js
+++ pages/origins/[slug].js
@@ -3,122 +3,118 @@ import { MDXRemote } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useInView } from 'react-intersection-observer';
+import Card from '../../components/Card';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import mapImage from '../../public/static/images/map.png';
 import { getAllStories } from '../../utils/data';
 
 export default function Article({
-    name,
-    date,
-    header,
-    username,
-    picture,
-    excerpt,
-    interviewer,
-    content,
+	name,
+	date,
+	header,
+	username,
+	featured,
+	picture,
+	excerpt,
+	interviewer,
+	content,
 }) {
-    return (
-        <>
-            <Head>
-                <title>{name} | Raid Guid Origins</title>
-                <meta name="description" content={excerpt} />
+	const { ref, inView } = useInView({ threshold: 0.05, triggerOnce: true });
+	const props = {
+		picture: picture,
+		name: name,
+		excerpt: excerpt,
+		featured: featured,
+		inView: inView,
+	};
+	return (
+		<>
+			<Head>
+				<title>{name} | Raid Guid Origins</title>
+				<meta name='description' content={excerpt} />
 
-                {/* Twitter */}
-                <meta
-                    property="twitter:card"
-                    content="summary_large_image"
-                    key="twcard"
-                />
+				{/* Twitter */}
+				<meta
+					property='twitter:card'
+					content='summary_large_image'
+					key='twcard'
+				/>
 
-                {/* Facebook */}
-                <meta
-                    property="og:site_name"
-                    content="Raid Guild Origins"
-                    key="ogsitename"
-                />
-                <meta
-                    property="og:url"
-                    content="https://portfolio.raidguild.org/"
-                    key="ogurl"
-                />
-                <meta
-                    property="og:image"
-                    content={`/story/${header}`}
-                    key="ogimage"
-                />
-                <meta
-                    property="og:title"
-                    content={`${name} | Raid Guild Origins`}
-                    key="ogtitle"
-                />
-                <meta
-                    property="og:description"
-                    content={excerpt}
-                    key="ogdesc"
-                />
-            </Head>
-            <div className="bg-dark-darker">
-                <div className="relative">
-                    <div className="relative z-10">
-                        <Header />
-                        <div className="px-4 md:px-40 lg:px-56 pt-4">
-                            <div className="aspect-w-11 aspect-h-5 filter drop-shadow-lg max-h-[650px]">
-                                <motion.div
-                                    animate={{
-                                        y: 0,
-                                        rotate: 0,
-                                        opacity: 1,
-                                    }}
-                                    initial={{
-                                        y: -100,
-                                        rotate: -6,
-                                        opacity: 0,
-                                    }}
-                                    transition={{
-                                        duration: 1.2,
-                                        delay: 1,
-                                    }}
-                                >
-                                    <Image
-                                        className="object-cover object-center rounded-md"
-                                        src={`/story/${header}`}
-                                        alt={`${name} header illustration`}
-                                        layout="fill"
-                                        objectFit="cover"
-                                        sizes="70vw"
-                                        priority
-                                    />
-                                </motion.div>
-                            </div>
-                        </div>
-                    </div>
-                    <Image
-                        src={mapImage}
-                        className="object-cover object-bottom absolute inset-0 z-0"
-                        layout="fill"
-                        sizes="70vw"
-                        quality="60"
-                        draggable="false"
-                        alt="Raid map"
-                    />
-                    <div className="absolute inset-0 bg-gradient-to-b from-dark via-primary opacity-80 z-0"></div>
-                </div>
-                <div className="flex justify-center md:px-16 lg:px-20 xl:px-24 gap-16 pt-8 h-full">
-                    <main className="self-center z-[999] px-6">
-                        <h1 className="font-sans">
-                            The Origin Story of {name}
-                        </h1>
-                        <p className="text-base">
-                            <span className="opacity-70">
-                                — From a talk with{' '}
-                            </span>
-                            <span className="text-primary opacity-100">
-                                {interviewer}
-                            </span>
-                        </p>
+				{/* Facebook */}
+				<meta
+					property='og:site_name'
+					content='Raid Guild Origins'
+					key='ogsitename'
+				/>
+				<meta
+					property='og:url'
+					content='https://portfolio.raidguild.org/'
+					key='ogurl'
+				/>
+				<meta property='og:image' content={`/story/${header}`} key='ogimage' />
+				<meta
+					property='og:title'
+					content={`${name} | Raid Guild Origins`}
+					key='ogtitle'
+				/>
+				<meta property='og:description' content={excerpt} key='ogdesc' />
+			</Head>
+			<div className='bg-dark-darker'>
+				<div className='relative'>
+					<div className='relative z-10'>
+						<Header />
+						<div className='px-4 md:px-40 lg:px-56 pt-4'>
+							<div className='aspect-w-11 aspect-h-5 filter drop-shadow-lg max-h-[650px]'>
+								<motion.div
+									animate={{
+										y: 0,
+										rotate: 0,
+										opacity: 1,
+									}}
+									initial={{
+										y: -100,
+										rotate: -6,
+										opacity: 0,
+									}}
+									transition={{
+										duration: 1.2,
+										delay: 1,
+									}}>
+									<Image
+										className='object-cover object-center rounded-md'
+										src={`/story/${header}`}
+										alt={`${name} header illustration`}
+										layout='fill'
+										objectFit='cover'
+										sizes='70vw'
+										priority
+									/>
+								</motion.div>
+							</div>
+						</div>
+					</div>
+					<Image
+						src={mapImage}
+						className='object-cover object-bottom absolute inset-0 z-0'
+						layout='fill'
+						sizes='70vw'
+						quality='60'
+						draggable='false'
+						alt='Raid map'
+					/>
+					<div className='absolute inset-0 bg-gradient-to-b from-dark via-primary opacity-80 z-0'></div>
+				</div>
+				<div className='flex flex-wrap justify-center md:px-16 lg:px-20 gap-16 pt-8 h-full'>
+					<main className='order-2 xl:order-1 self-center z-[999] px-6'>
+						<h1 className='font-sans'>The Origin Story of {name}</h1>
+						<p className='text-base'>
+							<span className='opacity-70'>— From a talk with </span>
+							<span className='text-primary opacity-100'>{interviewer}</span>
+						</p>
 
-                        {/* <div className="py-4 w-1/2">
+						{/* <div className="py-4 w-1/2">
                             <p>Listen the episode 👂</p>
                             <div className="flex justify-between text-sm text-grey-darker">
                                 <p>0:42</p>
@@ -133,83 +129,44 @@ export default function Article({
                             </div>
                         </div> */}
 
-                        <article className="prose prose-lg my-8 font-sans">
-                            <MDXRemote {...content} />
-                        </article>
-                    </main>
+						<article className='prose prose-lg my-8 font-sans'>
+							<MDXRemote {...content} />
+						</article>
+					</main>
 
-                    {/* <aside className="col-span-1 h-screen sticky top-4">
-                        <div className="rounded-md bg-white bg-opacity-10 ring-2 ring-gray-400 ring-inset backdrop-filter backdrop-blur-xl p-6 shadow-lg flex flex-col">
-                            <div className="flex justify-between items-center mb-4">
-                                <div className="rounded-full border-solid border-white border-opacity-50 border-2 relative h-20 w-20 overflow-hidden">
-                                    <Image
-                                        src={picture}
-                                        className="object-cover object-center"
-                                        layout="fill"
-                                        alt="Picture of the author"
-                                    />
-                                </div>
-                                <p className="rounded-md text-xs px-2 py-1 bg-white bg-opacity-30">
-                                    ⚔ RAIDER
-                                </p>
-                            </div>
-
-                            <p className=" text-xl font-semibold font-sans tracking-tight mb-2">
-                                {name}
-                            </p>
-
-                            <div className="flex flex-wrap gap-1 items-center mb-4 text-grey-200">
-                                <p className="rounded-md text-xs px-2 py-1 bg-white bg-opacity-10">
-                                    Visual Design
-                                </p>
-                                <p className="rounded-md text-xs px-2 py-1 bg-white bg-opacity-10">
-                                    Frontend Dev
-                                </p>
-                                <p className="rounded-md text-xs px-2 py-1 bg-white bg-opacity-10">
-                                    UX Design
-                                </p>
-                                <p className="rounded-md text-xs px-2 py-1 bg-white bg-opacity-10">
-                                    Smart Contract Dev
-                                </p>
-                            </div>
-
-                            <p className="text-sm opacity-70 leading-6 line-clamp-5">
-                                With DAOs, you work closely with someone and you
-                                get to see how they conduct themselves... and
-                                maybe they&apos;re still anonymous, but you
-                                learn to trust them and they build a reputation
-                                with you
-                            </p>
-                        </div>
-                    </aside> */}
-                </div>
-                <Footer />
-            </div>
-        </>
-    );
+					<aside
+						ref={ref}
+						className='order-1 xl:order-2 xl:h-screen xl:sticky top-4 px-6 lg:px-0 lg:pb-0'>
+						<Card props={props} />
+					</aside>
+				</div>
+				<Footer />
+			</div>
+		</>
+	);
 }
 
 export async function getStaticProps(context) {
-    const { params } = context;
-    const allStories = getAllStories();
+	const { params } = context;
+	const allStories = getAllStories();
 
-    const { data, content } = allStories.find(
-        (story) => story.slug === params.slug
-    );
-    const mdxSource = await serialize(content);
+	const { data, content } = allStories.find(
+		(story) => story.slug === params.slug
+	);
+	const mdxSource = await serialize(content);
 
-    return {
-        props: { ...data, content: mdxSource },
-    };
+	return {
+		props: { ...data, content: mdxSource },
+	};
 }
 
 export async function getStaticPaths() {
-    return {
-        paths: getAllStories().map((story) => ({
-            params: {
-                slug: story.slug,
-            },
-        })),
-        fallback: false,
-    };
+	return {
+		paths: getAllStories().map((story) => ({
+			params: {
+				slug: story.slug,
+			},
+		})),
+		fallback: false,
+	};
 }
