f76f72f8a057b1ffe647695d14d54c8a9e2b447a
Kevin Choi <kevinyoungchoi@gmail.com>
2021-11-08 21:37:05 -0500

    update footer style and add copyright

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,8 @@
 const Footer = () => {
     return (
-        <footer className="flex items-center justify-center h-16 px-8 bg-gradient-to-b from-dark via-primary opacity-80">
+        <footer className="flex flex-col items-center justify-center h-16 p-12 bg-gradient-to-r from-dark opacity-80">
             <p>Made with ⚔️ for our fellow raiders</p>
+            <p className="text-sm pt-2 text-opacity-50">&copy; {new Date().getFullYear()} Raid Guild. All rights reserved.</p>
         </footer>
     );
 };
